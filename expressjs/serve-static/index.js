c7bdb77fd25ae19cfe7856266301b2cd0a3958de
Earl Barr <E.Barr@ucl.ac.uk>
2016-03-02 14:36:14 +0000

    annotation

--- expressjs/serve-static/index.js
+++ expressjs/serve-static/index.js
@@ -1,4 +1,3 @@
-/* @flow */
 /*!
  * Connect - static
  * Copyright(c) 2010 Sencha Inc.
@@ -6,14 +5,13 @@
  * Copyright(c) 2014 Douglas Christopher Wilson
  * MIT Licensed
  */
-
+"use strict";
 /**
  * Module dependencies.
  */
-
+/// <reference path="node.d.ts"/>
 var send = require('send');
 var url = require('url');
-
 /**
  * Static:
  *
@@ -42,60 +40,53 @@ var url = require('url');
  * @return {Function}
  * @api public
  */
-
-exports = module.exports = function(root, options){
-  options = options || {};
-
-  // root required
-  if (!root) throw new TypeError('root path required');
-
-  // default redirect
-  var redirect = false !== options.redirect;
-
-  return function staticMiddleware(req, res, next) {
-    if ('GET' != req.method && 'HEAD' != req.method) return next();
-    var originalUrl = url.parse(req.originalUrl);
-    var path = parse(req).pathname;
-
-    if (path == '/' && originalUrl.pathname[originalUrl.pathname.length - 1] != '/') {
-      return directory();
-    }
-
-    function directory() {
-      if (!redirect) return next();
-      var target;
-      originalUrl.pathname += '/';
-      target = url.format(originalUrl);
-      res.statusCode = 303;
-      res.setHeader('Location', target);
-      res.end('Redirecting to ' + escape(target));
-    }
-
-    function error(err) {
-      if (404 == err.status) return next();
-      next(err);
-    }
-
-    send(req, path)
-      .maxage(options.maxAge || 0)
-      .root(root)
-      .index(options.index || 'index.html')
-      .hidden(options.hidden)
-      .on('error', error)
-      .on('directory', directory)
-      .pipe(res);
-  };
+exports = module.exports = function (root, options) {
+    options = options || {};
+    // root required
+    if (!root)
+        throw new TypeError('root path required');
+    // default redirect
+    var redirect = false !== options.redirect;
+    return function staticMiddleware(req, res, next) {
+        if ('GET' != req.method && 'HEAD' != req.method)
+            return next();
+        var originalUrl = url.parse(req.originalUrl);
+        var path = parse(req).pathname;
+        if (path == '/' && originalUrl.pathname[originalUrl.pathname.length - 1] != '/') {
+            return directory();
+        }
+        function directory() {
+            if (!redirect)
+                return next();
+            var target;
+            originalUrl.pathname += '/';
+            target = url.format(originalUrl);
+            res.statusCode = 303;
+            res.setHeader('Location', target);
+            res.end('Redirecting to ' + escape(target));
+        }
+        function error(err) {
+            if (404 == err.status)
+                return next();
+            next(err);
+        }
+        send(req, path)
+            .maxage(options.maxAge || 0)
+            .root(root)
+            .index(options.index || 'index.html')
+            .hidden(options.hidden)
+            .on('error', error)
+            .on('directory', directory)
+            .pipe(res);
+    };
 };
-
 /**
  * Expose mime module.
  *
  * If you wish to extend the mime table use this
  * reference to the "mime" module in the npm registry.
  */
-
 exports.mime = send.mime;
-
 /**
  * Escape the given string of `html`.
  *
@@ -103,15 +94,14 @@ exports.mime = send.mime;
  * @return {String}
  * @api private
  */
-
 function escape(html) {
-  return String(html)
-    .replace(/&(?!\w+;)/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
-};
-
+    return String(html)
+        .replace(/&(?!\w+;)/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+}
+;
 /**
  * Parse the `req` url.
  *
@@ -119,14 +109,12 @@ function escape(html) {
  * @return {Object}
  * @api private
  */
-
 function parse(req) {
-  var parsed = url.parse(req.url);
-
-  if (parsed.auth && !parsed.protocol && ~parsed.href.indexOf('//')) {
-    // This parses pathnames, and a strange pathname like //r@e should work
-    parsed = url.parse(req.url.replace(/@/g, '%40'));
-  }
-
-  return parsed;
-};
+    var parsed = url.parse(req.url);
+    if (parsed.auth && !parsed.protocol && ~parsed.href.indexOf('//')) {
+        // This parses pathnames, and a strange pathname like //r@e should work
+        parsed = url.parse(req.url.replace(/@/g, '%40'));
+    }
+    return parsed;
+}
+;
